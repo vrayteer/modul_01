413a22764f517a9a91a91980f86e624559dc9d37
vrayteer <azyomi8@gmail.com>
2023-09-11 17:56:02 +0300

    обновили js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const product = 'sofa';
-const amount = 5;
-const category = 'furniture';
-const price = 7000;
+{
+  const product = 'sofa';
+  const amount = 5;
+  const category = 'furniture';
+  const price = 7000;
 
-console.log('product: ', product);
-console.log((5 * 7000));
+  console.log('product: ', product);
+  console.log(`общая сумма товара: ${amount * price}`);
+}
 
+{
+  const product = 'table';
+  const amount = 20;
+  const category = 'furniture';
+  const price = 3500;
 
+  console.log('product: ', product);
+  console.log(`общая сумма товара: ${amount * price}`);
+}
