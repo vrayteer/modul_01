7c7b57c1e906dc3aeeda92ffd02c16891a4c8fa4
vrayteer <azyomi8@gmail.com>
2023-09-29 18:44:07 +0300

    lesson11

--- cart.js
+++ cart.js
@@ -10,9 +10,9 @@ const cart = {
 
   add(name, price, quantity = 1) {
     const newItem = {
-      name: name,
-      price: price,
-      quantity: quantity,
+      name,
+      price,
+      quantity,
     };
     this.items.push(newItem);
     this.increaseCount(quantity);
@@ -23,7 +23,8 @@ const cart = {
   },
 
   calculateItemPrice() {
-    return this.items.reduce((total, item) => total + item.price * item.quantity, 0); 
+    return this.items.reduce(
+      (total, item) => total + item.price * item.quantity, 0);
   },
 
   clear() {
@@ -35,12 +36,12 @@ const cart = {
   print() {
     console.log(JSON.stringify(this.items));
     console.log(`Общая стоимость корзины: ${this.totalPrice}`);
-  }
+  },
 };
 
-cart.add("Товар 1", 10);
-cart.add("Товар 2", 15, 3);
-cart.add("Товар 3", 8);
+cart.add('Товар 1', 10);
+cart.add('Товар 2', 15, 3);
+cart.add('Товар 3', 8);
 
 
 cart.print();
--- cart01.js
+++ cart01.js
@@ -1,28 +1,22 @@
 'use strict';
 
-// Функция для подсчета количества уникальных IP-адресов
 function countUniqueIPs(ipList) {
-  // Создаем пустой объект для хранения уникальных IP-адресов
   const uniqueIPs = {};
 
-  // Проходим по каждому IP-адресу в списке
   for (const ip of ipList) {
-      // Записываем IP-адрес как свойство объекта, чтобы убедиться, что он уникален
-      uniqueIPs[ip] = true;
+    uniqueIPs[ip] = true;
   }
 
-  // Возвращаем количество уникальных IP-адресов
   return Object.keys(uniqueIPs).length;
 }
 
-// Список IP-адресов
 const ipAddresses = [
-  "192.168.1.1",
-  "192.168.1.2",
-  "192.168.1.1",
-  "192.168.1.3",
-  "192.168.1.2"
+  '192.168.1.1',
+  '192.168.1.2',
+  '192.168.1.1',
+  '192.168.1.3',
+  '192.168.1.2',
 ];
 
 const uniqueCount = countUniqueIPs(ipAddresses);
-console.log("Количество уникальных IP-адресов:", uniqueCount);
+console.log('Количество уникальных IP-адресов:', uniqueCount);
